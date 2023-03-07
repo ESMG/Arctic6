a3feb2d40585bb69e56af9b385eae6e31f336a56
Kate Hedstrom <kshedstrom@alaska.edu>
2023-03-06 17:49:53 -0900

    Saving the HYCOM1 config at the top level.

--- mid_12/restart.py
+++ mid_12/restart.py
@@ -3,7 +3,7 @@ import os.path
 import netCDF4
 
 num = input("Restart number? ")
-file_path = "$AKDRIVE/Arctic_12k/run11/restart_" + num
+file_path = "$AKDRIVE/Arctic_12k/run19/restart_" + num
 if os.path.exists(file_path):
     print("directory exists already:", file_path)
     exit()
@@ -16,4 +16,4 @@ subprocess.call([cmd], shell=True)
 cmd = "mv RESTART/* INPUT"
 subprocess.call([cmd], shell=True)
 #vi ocean_arctic4.in
-#mv 201*.nc $AKDRIVE/Arctic_12k/run11
+#mv 201*.nc $AKDRIVE/Arctic_12k/run19
