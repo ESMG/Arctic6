d05cf3221cb5dca0bc244dc0618c3b5c63dcb845
Kate Hedstrom <kshedstrom@alaska.edu>
2024-05-30 21:13:30 -0800

    New answers without Laplacian

--- mid_12/test_eos_full/subset_boem.bash
+++ mid_12/test_eos_full/subset_boem.bash
@@ -1,1 +1,2 @@
-ncks -d lat,174,342 -d lon,217,394 -v u_atm,v_atm,ssu_east,ssv_north,ui_east,vi_north 19930101.boem_hourly.nc 19930101.boem_subset.nc
+#ncks -d yT,174,342 -d xT,217,394 -d yh,174,342 -d xh,217,394 -d lat,174,342 -d lon,217,394 -v u_atm,v_atm,ssu_east,ssv_north,ui_east,vi_north 19930101.boem_hourly.nc 19930101.boem_subset.nc
+ncks -d lath,174,342 -d lonh,217,394 -d latq,174,343 -d lonq,217,395 sea_ice_geometry.nc geometry_subset.nc
